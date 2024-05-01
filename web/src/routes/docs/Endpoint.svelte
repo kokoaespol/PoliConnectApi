d66ba0e7ab5946ab88546b60cea8a309a04ce136
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-04-30 20:53:48 -0600

    CHORE: Copiar en el portapapeles

--- web/src/routes/docs/Endpoint.svelte
+++ web/src/routes/docs/Endpoint.svelte
@@ -1,6 +1,4 @@
 <script>
-    import { onMount } from 'svelte';
-
     import EndpointParams from './EndpointParams.svelte';
     import EndpointResponse from './EndpointResponse.svelte';
 
@@ -10,35 +8,11 @@
     export let params = [];
     export let responses = [];
 
-    let showPopup = false;
-    let popupX = 0;
-    let popupY = 0;
-
-    function copyToClipboard(text) {
-        navigator.clipboard.writeText(text)
-            .then(() => {
-                showPopup = true;
-                setTimeout(() => {
-                    showPopup = false;
-                }, 1500); // 1.5s popup
-            })
-            .catch(err => {
-                console.error('Error al copiar al portapapeles: ', err);
-            });
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text)      
     }
-
-    onMount(() => {
-        const buttonRect = document.querySelector('.copy-button').getBoundingClientRect();
-        popupX = buttonRect.right + 10;
-        popupY = buttonRect.top - 10;
-    });
 </script>
 
-{#if showPopup}
-    <div style="top: {popupY}px; left: {popupX}px;" class="popup">
-        Copiado
-    </div>
-{/if}
 
 <h3 class="text-lg mb-2 font-bold">{title}</h3>
 <div class="flex justify-between items-center w-full bg-gray-900 font-mono my-4">
@@ -49,7 +23,7 @@
     </div>
     <button
         on:click={() => copyToClipboard(`${method} ${endpoint}`)}
-        class="copy-button cursor-pointer"
+        class="cursor-pointer"
     >
         <i class="fa-solid fa-clipboard mr-2"></i>
     </button>
@@ -66,14 +40,3 @@
         />
     {/each}
 </div>
-
-<style>
-    .popup {
-        position: fixed;
-        background-color: #4CAF50;
-        color: white;
-        padding: 10px;
-        border-radius: 5px;
-        box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-    }
-</style>
